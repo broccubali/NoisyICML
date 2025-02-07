6e823f420ea9de7e242581239eda4b7deb56f316
Shusrith S <116418999+MBUYt0n@users.noreply.github.com>
2025-02-07 23:36:43 +0530

    forward

--- metrics/mse-calc.py
+++ metrics/mse-calc.py
@@ -4,22 +4,22 @@ import numpy as np
 with open("results.json", "r") as f:
     results = json.load(f)
 
-c = []
-n = []
-e = []
+cmse = []
+nmse = []
 cl = []
 nl = []
+
 for result in results:
-    e.append(results[result][0]["epsilon"])
-    c.append(results[result][1]["clean"][0]["predicted"])
-    n.append(results[result][2]["noisy"][0]["predicted"])
+    cmse.append(results[result][1]["clean"][0]["mse"])
+    nmse.append(results[result][2]["noisy"][0]["mse"])
     cl.append(results[result][1]["clean"][0]["loss"])
     nl.append(results[result][2]["noisy"][0]["loss"])
 
-e = np.array(e)
-c = np.array(c)
-n = np.array(n)
-print("MSE of predicted solution of clean data", np.mean((e - c) ** 2))
-print("MSE of predicted solution of noisy data", np.mean((e - n) ** 2))
-print("Mean of loss on clean data", np.mean(cl))
-print("Mean of loss on noisy data", np.mean(nl))
+cmse = np.array(cmse)
+nmse = np.array(nmse)
+cl = np.array(cl)
+nl = np.array(nl)
+print("Average MSE clean: ", np.mean(cmse))
+print("Average MSE noisy: ", np.mean(nmse))
+print("Average Loss clean: ", np.mean(cl))
+print("Average Loss noisy: ", np.mean(nl))
