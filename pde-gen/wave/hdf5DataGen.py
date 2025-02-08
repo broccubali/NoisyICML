70c3c2898ef8cf774a419fccaee23494e17b3c4a
Shusrith S <116418999+MBUYt0n@users.noreply.github.com>
2025-02-08 07:27:39 +0530

    fixed this

--- pde-gen/wave/hdf5DataGen.py
+++ pde-gen/wave/hdf5DataGen.py
@@ -2,7 +2,8 @@ import numpy as np
 import h5py
 from scipy.stats import skewnorm
 
-def newData(c, f):
+def newData(i, f):
+    c = np.random.uniform(0.5, 100)  # Wave speed
     x_min, x_max = -1.0, 1.0  # Spatial domain
     t_min, t_max = 0.0, 2.0   # Temporal domain
     num_x, num_t = 1024, 201   # Grid resolution
@@ -12,8 +13,8 @@ def newData(c, f):
     # Spatial and temporal grids
     x = np.linspace(x_min, x_max, num_x)
     t = np.linspace(t_min, t_max, num_t)
-
-    u0_clean = np.exp(-50 * (x**2))  # Initial displacement: Gaussian pulse
+    r = np.random.uniform(1, 100)
+    u0_clean = np.exp(-r * (x**2))  # Initial displacement: Gaussian pulse
     u1_clean = u0_clean.copy()  # Initial velocity: zero
     U_clean = np.zeros((num_x, num_t))  # Solution array
     U_clean[:, 0] = u0_clean
@@ -31,33 +32,20 @@ def newData(c, f):
             + c**2 * dt**2 / dx**2 * (U_clean[2:, n] - 2 * U_clean[1:-1, n] + U_clean[:-2, n])
         )
 
-    for n in range(1, num_t - 1):
-        U_noise[1:-1, n + 1] = (
-            2 * U_noise[1:-1, n] - U_noise[1:-1, n - 1]
-            + c**2 * dt**2 / dx**2 * (U_noise[2:, n] - 2 * U_noise[1:-1, n] + U_noise[:-2, n])
-        )
+    U_noise = U_clean + noise
 
     # Remove the singleton dimension
     U_clean = U_clean.squeeze()  # Remove the second dimension (of size 1)
     U_noise = U_noise.squeeze()  # Remove the second dimension (of size 1)
-
-    # Define the data type for the compound dataset (with fields 'clean' and 'noisy')
-    dt = np.dtype([
-        ('clean', np.float64, U_clean.shape),  # The shape of the clean data
-        ('noisy', np.float64, U_noise.shape)   # The shape of the noisy data
-    ])
-
-    # Create a compound dataset for each 'c' with 'clean' and 'noisy' fields
-    data = np.zeros(1, dtype=dt)
-    data['clean'] = U_clean
-    data['noisy'] = U_noise
-
-    # Store the compound dataset in the HDF5 file
-    f.create_dataset(f"wave_solution_{c}", data=data)
+    f.create_group(f"{i}")
+    f.create_dataset(f"{i}/clean", data=U_clean)
+    f.create_dataset(f"{i}/noisy", data=U_noise)
+    f.create_dataset(f"{i}/c", data=c)
+    f.create_dataset(f"{i}/init", data=r)
 
     print(f"Data for c = {c} generated and added to HDF5 file")
 
 # Open the HDF5 file once and write all datasets to it
-with h5py.File("/home/pes1ug22am100/Documents/Research and Experimentation/NoisyICML/pinns-inverse/WaveEquation/wave_solutions_new.h5", 'w') as f:
-    for c in range(1, 963):
-        newData(c, f)
+with h5py.File("data1.h5", 'w') as f:
+    for i in range(1, 500):
+        newData(i, f)
